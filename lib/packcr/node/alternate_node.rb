8f41d743810c10fe545d1d2f233f732f7adca64b
wanabe <s.wanabe@gmail.com>
2022-12-15 08:51:52 +0900

    Extract AlternateNode#generate_code code as ERB template

--- lib/packcr/node/alternate_node.rb
+++ lib/packcr/node/alternate_node.rb
@@ -24,57 +24,12 @@ class Packcr
         b = false
         m = gen.next_label
 
+        reach = nil
         gen.generate_block(indent, bare) do |indent|
-          gen.write " " * indent
-          gen.write "const size_t p = ctx->cur;\n"
-          gen.write " " * indent
-          gen.write "const size_t n = chunk->thunks.len;\n"
-
-          nodes.each_with_index do |expr, i|
-            c = i + 1 < nodes.length
-            l = gen.next_label
-            case gen.generate_code(expr, l, indent, false)
-            when Packcr::CODE_REACH__ALWAYS_SUCCEED
-              if c
-                gen.write " " * indent
-                gen.write "/* unreachable codes omitted */\n"
-              end
-              if b
-                if indent > 4
-                  gen.write " " * (indent - 4)
-                end
-                gen.write "L#{"%04d" % m}:;\n"
-              end
-              return Packcr::CODE_REACH__ALWAYS_SUCCEED
-            when Packcr::CODE_REACH__ALWAYS_FAIL
-            else
-              b = true
-              gen.write " " * indent
-              gen.write "goto L#{"%04d" % m};\n"
-            end
-
-            if indent > 4
-              gen.write " " * (indent - 4)
-            end
-            gen.write "L#{"%04d" % l}:;\n"
-            gen.write " " * indent
-            gen.write "ctx->cur = p;\n"
-            gen.write " " * indent
-            gen.write "pcc_thunk_array__revert(ctx->auxil, &chunk->thunks, n);\n"
-            if !c
-              gen.write " " * indent
-              gen.write "goto L#{"%04d" % onfail};\n"
-            end
-          end
-          if b
-            if indent > 4
-              gen.write " " * (indent - 4)
-            end
-            gen.write "L#{"%04d" % m}:;\n"
-          end
-
-          b ? Packcr::CODE_REACH__BOTH : Packcr::CODE_REACH__ALWAYS_FAIL
+          gen.write Packcr.template("node/alternate.c.erb", binding, indent: indent - 4)
         end
+
+        reach || Packcr::CODE_REACH__ALWAYS_FAIL
       end
     end
   end
